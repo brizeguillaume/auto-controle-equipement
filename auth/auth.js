349fa5e1c09cc8ca6e43a1b86035528e9b806adf
brizeguillaume <brizeguillaume@gmail.com>
2025-04-11 16:23:41 -0400

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,1 +1,20 @@
+const msalConfig = {
+    auth: {
+        clientId: "VOTRE_CLIENT_ID_AZURE",
+        authority: "https://login.microsoftonline.com/VOTRE_TENANT_ID",
+        redirectUri: window.location.origin + "/auth/auth.html"
+    }
+};
 
+async function login() {
+    const msalInstance = new msal.PublicClientApplication(msalConfig);
+    const loginRequest = {
+        scopes: ["Files.ReadWrite"]
+    };
+    
+    try {
+        await msalInstance.loginRedirect(loginRequest);
+    } catch (err) {
+        console.error("Erreur:", err);
+    }
+}
