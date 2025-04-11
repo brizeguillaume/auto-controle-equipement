ee40a98e497193d75bc47e87a1328e8129bd68b3
brizeguillaume <brizeguillaume@gmail.com>
2025-04-11 16:25:17 -0400

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -18,3 +18,19 @@ async function login() {
         console.error("Erreur:", err);
     }
 }
+// Dans auth.js
+async function getToken() {
+    const msalInstance = new msal.PublicClientApplication(msalConfig);
+    const accounts = msalInstance.getAllAccounts();
+    
+    if (accounts.length > 0) {
+        const silentRequest = { account: accounts[0] };
+        try {
+            const response = await msalInstance.acquireTokenSilent(silentRequest);
+            return response.accessToken;
+        } catch {
+            return await login();
+        }
+    }
+    return null;
+}
